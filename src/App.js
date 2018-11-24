1cc9c494dc8ea09a93d076ccdb956c05486b8118
dqisme <dqisme@outlook.com>
2018-11-25 00:38:01 +0800

    [dq] add header and product list page in app.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
+  Route,
 } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import ProductListPage from './pages/ProductList/ProductListPage';
 
 class App extends Component {
   render() {
     return (
-      <Router></Router>
+      <Router>
+        <div>
+          <Header />
+          <Route path="/" component={ProductListPage} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/pages/ProductList/ProductListPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ProductListPage extends Component {
+  render() {
+    return (
+      <div>
+        This is Product List Page.
+      </div>
+    );
+  }
+}
+
+export default ProductListPage;
