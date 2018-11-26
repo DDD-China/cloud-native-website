0b118e110cc20be58038d984fc6e2e2d82dfbcd9
dqisme <dqisme@outlook.com>
2018-11-26 13:04:36 +0800

    [dq] add login page.

---
+++ src/pages/Registry/RegistryPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({});
+
+class RegistryPage extends Component {
+  render() {
+    return (
+      <div>
+        This is Registry Page.
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(RegistryPage);
