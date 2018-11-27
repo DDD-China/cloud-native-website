6710c3349d098a5fb51a2679b21b9b1447b68cd8
dqisme <dqisme@outlook.com>
2018-11-28 00:09:09 +0800

    [dq] add address form and order review in check out page.

--- src/components/AddressForm/AddressForm.js
+++ src/components/AddressForm/AddressForm.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
 
 const styles = theme => ({});
 
 class AddressForm extends Component {
   render() {
     return (
-      <div>
-        This is AddressForm.
-      </div>
+      <>
+        <Typography variant="h6" gutterBottom>
+          Address Information
+        </Typography>
+        <Grid container spacing={24}>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="city"
+              name="city"
+              label="City"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="zip"
+              name="zip"
+              label="Zip / Postal code"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              id="country"
+              name="country"
+              label="Country"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              id="address"
+              name="address"
+              label="Address line"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+      </>
     );
   }
 }
--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Grid from '@material-ui/core/Grid';
 
-const styles = theme => ({});
+const styles = theme => ({
+  listItem: {
+    padding: `${theme.spacing.unit}px 0`,
+  },
+});
+
+const products = [
+  { name: 'Product 1', desc: 'This is a product description', price: 123, amount: 1 },
+];
+
+const addresses = ['1 Material-UI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
 
 class OrderReview extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        This is OrderReview.
-      </div>
+      <>
+        <Typography variant="h6" gutterBottom>
+          Order summary
+        </Typography>
+        <List disablePadding>
+          {products.map(product => (
+            <ListItem className={classes.listItem} key={product.name}>
+              <ListItemText primary={product.name} secondary={product.desc} />
+              <Grid direction="column">
+                <Typography align="right">x{product.amount}</Typography>
+                <Typography align="right">¥{product.price.toFixed(2)}</Typography>
+              </Grid>
+            </ListItem>
+          ))}
+          <ListItem className={classes.listItem}>
+            <ListItemText primary="Total" />
+            <Typography variant="subtitle1" className={classes.total}>
+              ¥123.00
+            </Typography>
+          </ListItem>
+        </List>
+        <Typography variant="h6" gutterBottom className={classes.title}>
+          Shipping
+        </Typography>
+        <Typography gutterBottom>{addresses.join(', ')}</Typography>
+      </>
     );
   }
 }
